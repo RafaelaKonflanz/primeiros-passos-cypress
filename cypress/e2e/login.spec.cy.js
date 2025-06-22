0d034b3ab6e77a5df21e7912a74dcf2fa68e7939
Rafaela Konflanz <you@example.com>
2025-06-21 21:24:59 -0300

    i "base url added"

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -12,7 +12,7 @@ describe('orange hrm tests', () => {
     }
 
   it('Login success ', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
