14deedb96b191e04a23c3d8095c8e43b09b3cad7
Rafaela Konflanz <you@example.com>
2025-07-01 18:18:13 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,32 +1,37 @@
 import userData from '../fixtures/userData.json'
+import LoginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js'
+import enuPage from '../pages/menuPage.js'
+import MenuPage from '../pages/menuPage.js'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
+
+
 
 describe('orange hrm tests', () => {
 
     const selectorsList = {
-      usernameField: "[name='username']",
-      passwordField: "[name='password']",
-      loginButton: "[type='submit']",
-      sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-      dashboardGrid: ".orangehrm-dashboard-grid",
-      wrongCredentialAlert: "[role='alert']",
-      myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+               
       firstNameField: "[name='firstName']",
       lastNameField: "[name='lastName']",
       genericField: ".oxd-input--active",
       dateField: "[placeholder='yyyy-dd-mm']",
+      genericCombobox: ".oxd-select-text--active",
+      secondItemCombobox: ".oxd-select-dropdown > :nth-child(2)",
+      thirdItemCombobox: ".oxd-select-dropdown > :nth-child(3)",
       dateCloseButton: ".--close",
-      submitButton: "[type='submit']"
+      submitButton: "[type='submit']"      
     }
 
   it.only('User Info Update - success ', () => {
-
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click()
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+    
+    dashboardPage.checkDashboardPage()
+    menuPage.accessMyInfo()
+    
     cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
     cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
     cy.get(':nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('Employee')
@@ -34,8 +39,15 @@ describe('orange hrm tests', () => {
     cy.get(':nth-child(2) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('DriverLice')
     cy.get(selectorsList.genericField).eq(7).clear().type('2025-06-29')
     cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get(selectorsList.submitButton).eq(0).click({ force: true})
     cy.get('body').should('contain','Successfully Updated')
+    cy.get('.oxd-toast-close')
+
+    cy.get(selectorsList.genericCombobox).eq(0).click()
+    cy.get(selectorsList.secondItemCombobox).click()
+    cy.get(selectorsList.genericCombobox).eq(1).click()
+    cy.get(selectorsList.thirdItemCombobox).click()
+    
     
   })
   it('Login Fail ', () => {
